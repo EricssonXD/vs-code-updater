b0e6ae7c68ae8df9aa029a47fd6adb90cfa51347
EricssonXD <114271127+EricssonXD@users.noreply.github.com>
2025-02-06 12:40:01 +0800

    test detect package manager

--- src/extension.ts
+++ src/extension.ts
@@ -1,18 +1,36 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import { exec } from 'child_process';
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+    console.log('Congratulations, your extension is now active!');
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "vs-code-updator" is now active!');
+    // Register a command to update VSCode with the correct Linux package manager
+    const updatePackageManagerCommand = vscode.commands.registerCommand('vs-code-updator.updateLinuxPackageManager', () => {
+        // Check for apt
+        exec('command -v apt', (error, stdout, stderr) => {
+            if (stdout && stdout.trim().length > 0) {
+                vscode.window.showInformationMessage('Detected package manager: apt');
+            } else {
+                // Check for pacman if apt not found
+                exec('command -v pacman', (error2, stdout2, stderr2) => {
+                    if (stdout2 && stdout2.trim().length > 0) {
+                        vscode.window.showInformationMessage('Detected package manager: pacman');
+                    } else {
+                        // Check for dnf if neither apt nor pacman found
+                        exec('command -v dnf', (error3, stdout3, stderr3) => {
+                            if (stdout3 && stdout3.trim().length > 0) {
+                                vscode.window.showInformationMessage('Detected package manager: dnf');
+                            } else {
+                                vscode.window.showInformationMessage('Package manager not detected');
+                            }
+                        });
+                    }
+                });
+            }
+        });
+    });
+    context.subscriptions.push(updatePackageManagerCommand);
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
 	const disposable = vscode.commands.registerCommand('vs-code-updator.helloWorld', () => {
 		// The code you place here will be executed every time your command is executed
 		// Display a message box to the user
@@ -22,5 +40,4 @@ export function activate(context: vscode.ExtensionContext) {
 	context.subscriptions.push(disposable);
 }
 
-// This method is called when your extension is deactivated
 export function deactivate() {}
